9ff04cf179bea8c37bf09ada3211c8f17ff3bc33
Chunpeng Huo <huocp@me.com>
2018-04-11 10:34:09 +1000

    feat: shim support.

--- spec/defines.spec.js
+++ spec/defines.spec.js
@@ -244,3 +244,102 @@ test('defines inserts correctly for cjs wrapper define case 3', t => {
   t.end();
 });
 
+test('defines shim', t => {
+  const shim = 'var Foo = "Foo";';
+  const shimExpected = 'var Foo = "Foo";\n' +
+                       'define("shim", [\'bar\'], (function (global) {\n' +
+                       '  return function () {\n' +
+                       '    return global.Foo;\n' +
+                       '  };\n' +
+                       '}(this)));\n';
+  const r = defines('shim', shim, {deps: ['bar'], 'exports': 'Foo'});
+  t.equal(r.defined, 'shim');
+  t.equal(r.contents, shimExpected);
+  t.end();
+});
+
+test('defines shim without deps', t => {
+  const shim = 'var Foo = "Foo";';
+  const shimExpected = 'var Foo = "Foo";\n' +
+                       'define("shim", (function (global) {\n' +
+                       '  return function () {\n' +
+                       '    return global.Foo;\n' +
+                       '  };\n' +
+                       '}(this)));\n';
+  const r = defines('shim', shim, {'exports': 'Foo'});
+  t.equal(r.defined, 'shim');
+  t.equal(r.contents, shimExpected);
+  t.end();
+});
+
+test('defines shim without exports', t => {
+  const shim = 'var Foo = "Foo";';
+  const shimExpected = 'var Foo = "Foo";\n' +
+                       'define("shim", [\'bar\'], function(){});\n';
+  const r = defines('shim', shim, {deps: ['bar']});
+  t.equal(r.defined, 'shim');
+  t.equal(r.contents, shimExpected);
+  t.end();
+});
+
+test('defines wrapShim', t => {
+  const shim = 'var Foo = "Foo";';
+  const shimExpected = '(function(root) {\n' +
+                       'define("shim", [\'bar\'], function() {\n' +
+                       '  return (function() {\n' +
+                       'var Foo = "Foo";\n' +
+                       'return root.Foo = Foo;\n' +
+                       '  }).apply(root, arguments);\n' +
+                       '});\n' +
+                       '}(this));\n';
+  const r = defines('shim', shim, {deps: ['bar'], 'exports': 'Foo', wrapShim: true});
+  t.equal(r.defined, 'shim');
+  t.equal(r.contents, shimExpected);
+  t.end();
+});
+
+test('defines wrapShim without deps', t => {
+  const shim = 'var Foo = "Foo";';
+  const shimExpected = '(function(root) {\n' +
+                       'define("shim", function() {\n' +
+                       '  return (function() {\n' +
+                       'var Foo = "Foo";\n' +
+                       'return root.Foo = Foo;\n' +
+                       '  }).apply(root, arguments);\n' +
+                       '});\n' +
+                       '}(this));\n';
+  const r = defines('shim', shim, {'exports': 'Foo', wrapShim: true});
+  t.equal(r.defined, 'shim');
+  t.equal(r.contents, shimExpected);
+  t.end();
+});
+
+test('defines wrapShim without exports', t => {
+  const shim = 'var Foo = "Foo";';
+  const shimExpected = '(function(root) {\n' +
+                       'define("shim", [\'bar\'], function() {\n' +
+                       '  return (function() {\n' +
+                       'var Foo = "Foo";\n' +
+                       '\n' +
+                       '  }).apply(root, arguments);\n' +
+                       '});\n' +
+                       '}(this));\n';
+  const r = defines('shim', shim, {deps: ['bar'], wrapShim: true});
+  t.equal(r.defined, 'shim');
+  t.equal(r.contents, shimExpected);
+  t.end();
+});
+
+test('defines ignores shim settings if source code already defined amd module', t => {
+  const good1 = 'if (typeof define === "function" && define.amd) {\n' +
+                '    define(definition);\n' +
+                '}';
+
+  const goodExpected1 = 'if (typeof define === "function" && define.amd) {\n' +
+                        '    define(\'good/1\',definition);\n' +
+                        '}';
+  const r = defines('good/1', good1, {deps: ['bar'], 'exports': 'Foo', wrapShim: true});
+  t.equal(r.defined, 'good/1');
+  t.equal(r.contents, goodExpected1);
+  t.end();
+});
