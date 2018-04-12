f58584b4b639bcc8a344ec1cbd4ad6cd14329c3e
Chunpeng Huo <huocp@me.com>
2018-04-12 13:35:02 +1000

    feat: default npm package locator.

---
+++ spec/default-npm-package-locator.spec.js
@@ -0,0 +1,106 @@
+'use strict';
+const test = require('tape');
+const defaultLocator = require('../lib/default-npm-package-locator');
+const mock = require('mock-fs');
+
+test('defaultNpmPackageLocator rejects missing package', t => {
+  mock();
+
+  defaultLocator('foo')
+  .then(
+    () => t.fail('should not pass'),
+    () => t.pass('reject missing package')
+  )
+  .then(() => {
+    mock.restore();
+    t.end();
+  });
+});
+
+test('defaultNpmPackageLocator returns fileRead func for existing package', t => {
+  mock({
+    'node_modules/foo/package.json': 'lorem'
+  });
+
+  defaultLocator('foo')
+  .then(
+    fileRead => {
+      return fileRead('package.json')
+      .then(
+        contents => t.equal(contents.toString(), 'lorem'),
+        err => t.fail(err.message)
+      );
+    },
+    () => t.fail('should not fail')
+  )
+  .then(() => {
+    mock.restore();
+    t.end();
+  });
+});
+
+test('defaultNpmPackageLocator returns fileRead func rejects missing file for existing package', t => {
+  mock({
+    'node_modules/foo/package.json': 'lorem'
+  });
+
+  defaultLocator('foo')
+  .then(
+    fileRead => {
+      return fileRead('nope')
+      .then(
+        () => t.fail('should not read non-existing file'),
+        () => t.pass('rejects missing file')
+      );
+    },
+    () => t.fail('should not fail')
+  )
+  .then(() => {
+    mock.restore();
+    t.end();
+  });
+});
+
+test('defaultNpmPackageLocator returns fileRead func for existing scoped package', t => {
+  mock({
+    'node_modules/@bar/foo/package.json': 'lorem'
+  });
+
+  defaultLocator('@bar/foo')
+  .then(
+    fileRead => {
+      return fileRead('package.json')
+      .then(
+        contents => t.equal(contents.toString(), 'lorem'),
+        err => t.fail(err.message)
+      );
+    },
+    () => t.fail('should not fail')
+  )
+  .then(() => {
+    mock.restore();
+    t.end();
+  });
+});
+
+test('defaultNpmPackageLocator returns fileRead func rejects missing file for existing scoped package', t => {
+  mock({
+    'node_modules/@bar/foo/package.json': 'lorem'
+  });
+
+  defaultLocator('@bar/foo')
+  .then(
+    fileRead => {
+      return fileRead('nope')
+      .then(
+        () => t.fail('should not read non-existing file'),
+        () => t.pass('rejects missing file')
+      );
+    },
+    () => t.fail('should not fail')
+  )
+  .then(() => {
+    mock.restore();
+    t.end();
+  });
+});
