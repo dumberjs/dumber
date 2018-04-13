5e3630bab18499dfbd03cfeb723cd1e22358fc1a
Chunpeng Huo <huocp@me.com>
2018-04-13 10:15:11 +1000

    feat: json transform into amd.

---
+++ spec/transformers/json.spec.js
@@ -0,0 +1,14 @@
+'use strict';
+const test = require('tape');
+const json = require('../../lib/transformers/json');
+
+test('json wraps json into amd module', t => {
+  const source = '{"a":1}';
+  const target = "define('text!a.json',function(){return \"{\\\"a\\\":1}\";});\n" +
+                 "define('a.json',['text!a.json'],function(m){return JSON.parse(m);});\n";
+  t.deepEqual(json('a.json', source), {
+    defined: ['text!a.json', 'a.json'],
+    contents: target
+  });
+  t.end();
+});
