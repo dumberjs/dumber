844e02ed42d9545f5a1e40e1a3c6fb5005653639
Chunpeng Huo <huocp@me.com>
2018-04-13 14:16:39 +1000

    feat: alias creates aliases.

---
+++ spec/transformers/alias.spec.js
@@ -0,0 +1,44 @@
+'use strict';
+const test = require('tape');
+const alias = require('../../lib/transformers/alias');
+
+test('alias creates aliases for js module', t => {
+  t.deepEqual(alias('from/id', 'to/id'), {
+    defined: 'from/id',
+    contents: "define('from/id',['to/id'],function(m){return m;});\n"
+  });
+  t.deepEqual(alias('from/id.js', 'to/id'), {
+    defined: 'from/id',
+    contents: "define('from/id',['to/id'],function(m){return m;});\n"
+  });
+  t.deepEqual(alias('from/id', 'to/id.js'), {
+    defined: 'from/id',
+    contents: "define('from/id',['to/id'],function(m){return m;});\n"
+  });
+  t.deepEqual(alias('from/id.js', 'to/id.js'), {
+    defined: 'from/id',
+    contents: "define('from/id',['to/id'],function(m){return m;});\n"
+  });
+  t.end();
+});
+
+test('alias creates aliases for other modules', t => {
+  t.deepEqual(alias('from/id.json', 'to/id.json'), {
+    defined: ['text!from/id.json', 'from/id.json'],
+    contents: "define('text!from/id.json',['text!to/id.json'],function(m){return m;});\n" +
+              "define('from/id.json',['to/id.json'],function(m){return m;});\n"
+  });
+  t.deepEqual(alias('from/id.css', 'to/id.css'), {
+    defined: ['text!from/id.css', 'from/id.css'],
+    contents: "define('text!from/id.css',['text!to/id.css'],function(m){return m;});\n" +
+              "define('from/id.css',['to/id.css'],function(m){return m;});\n"
+  });
+  t.end();
+});
+
+test('alias rejects alias between ids with different extname', t => {
+  t.throws(() => alias('from', 'to.json'));
+  t.throws(() => alias('from.js', 'to.json'));
+  t.throws(() => alias('from.html', 'to.htm'));
+  t.end();
+});
