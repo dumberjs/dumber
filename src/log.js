0b0e77b2ac443ad025ac148623263aa22070cb90
Chunpeng Huo <huocp@me.com>
2019-01-02 12:05:22 +1100

    chore: update log color

--- src/log.js
+++ src/log.js
@@ -1,11 +1,11 @@
 export function info(message) {
-  console.info('[dumber] INFO: ' + message);
+  console.info(`\x1b[36m[dumber] INFO:: ${message}\x1b[0m`);
 }
 
 export function warn(message) {
-  console.warn(`\x1b[43m\x1b[30m[dumber] WARN: ${message}\x1b[0m`);
+  console.warn(`\x1b[32m[dumber] WARN: ${message}\x1b[0m`);
 }
 
 export function error(message) {
-  console.error(`\x1b[41m\x1b[37m[dumber] ERROR: ${message}\x1b[0m`);
+  console.error(`\x1b[31m[dumber] ERROR: ${message}\x1b[0m`);
 }
