eeba6414b4da01e34d789be2e234898b2998c211
Chunpeng Huo <huocp@me.com>
2019-02-06 11:56:01 +1100

    test: browserify+@babel/core issue has getting worse

--- spec/all-browser-spec.js
+++ spec/all-browser-spec.js
@@ -5,10 +5,10 @@ require('./package.spec');
 require('./parser.uses-amd-or-requirejs.spec');
 require('./parser.uses-common-js.spec');
 require('./parser.uses-esm.spec');
-require('./trace.spec');
 require('./transformers/alias.spec');
 // something wrong with browserify + @babel/core
 // going to switch to dumberify when it's ready
+// require('./trace.spec');
 // require('./transformers/cjs-es.spec');
 require('./transformers/defines.spec');
 require('./transformers/text.spec');
