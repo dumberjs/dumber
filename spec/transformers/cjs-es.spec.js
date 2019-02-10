7d97723e54d5b14eff6dc358f36bb863aff2bb02
Chunpeng Huo <huocp@me.com>
2019-02-10 11:07:12 +1100

    chore: use Buffer.from

--- spec/transformers/cjs-es.spec.js
+++ spec/transformers/cjs-es.spec.js
@@ -87,10 +87,10 @@ test('cjsEs transform wraps ES module', t => {
 });
 
 test('cjsEs transform wraps cjs code with global, process, and Buffer', t => {
-  const source = 'exports.name = global.bar;exports.loo = new Buffer(process.cwd());';
+  const source = 'exports.name = global.bar;exports.loo = Buffer.from(process.cwd());';
   const expected = 'define(function (require, exports, module) {' +
                    'var global = this;var process = require(\'process\');var Buffer = require(\'buffer\').Buffer;\n' +
-                   'exports.name = global.bar;exports.loo = new Buffer(process.cwd());\n});\n';
+                   'exports.name = global.bar;exports.loo = Buffer.from(process.cwd());\n});\n';
 
   t.deepEqual(cjsEs(source), {headLines: 1, contents: expected});
   t.end();
