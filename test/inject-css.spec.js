2d11107c25a378871cb8fd20dde645178e93055b
Chunpeng Huo <huocp@me.com>
2019-09-02 09:36:29 +1000

    feat: support gulp-dumber-css-module
    closes #13

--- test/inject-css.spec.js
+++ test/inject-css.spec.js
@@ -1,5 +1,5 @@
 const test = require('tape');
-const {fixupCSSUrls} = require('../lib/inject-css');
+const {fixupCSSUrls, splitCssModuleExports} = require('../lib/inject-css');
 
 // tests partly copied from
 // https://github.com/webpack-contrib/style-loader/blob/master/test/fixUrls.test.js
@@ -160,5 +160,20 @@ test("fixupCSSUrls doesn't break inline SVG", t => {
 test("fixupCSSUrls doesn't break inline SVG with HTML comment", t => {
   const css = "body { background-image:url('data:image/svg+xml,%3C%3Fxml%20version%3D%221.0%22%20encoding%3D%22utf-8%22%3F%3E%0A%3C!--%20Comment%20--%3E%0A%3Csvg%3E%3C%2Fsvg%3E%0A'); }";
   t.equal(fixupCSSUrls('foo/bar', css), css);
+  t.end();
+});
+
+test('splitCssModuleExports returns original contents when css-module is not detected', t => {
+  t.deepEqual(splitCssModuleExports(), {css: ''});
+  t.deepEqual(splitCssModuleExports('.a { color: red; }'), {css: '.a { color: red; }'});
+  t.end();
+});
+
+test('splitCssModuleExports splits css-module exports', t => {
+  const css = '._a_1vkqw_1 { color: red; }\n/* dumber-css-module: {"a":"_a_1vkqw_1"} */\n';
+  t.deepEqual(splitCssModuleExports(css), {
+    css: '._a_1vkqw_1 { color: red; }\n',
+    exportTokens: {a: '_a_1vkqw_1'}
+  });
   t.end();
 });
