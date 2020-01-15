1a5bb493df0feced674ad2600dc40b84ae43d475
Chunpeng Huo <huocp@me.com>
2020-01-15 15:08:51 +1100

    refactor: resolve in browser

--- test/stub-module.spec.js
+++ test/stub-module.spec.js
@@ -1,18 +1,12 @@
 const test = require('tape');
-const path = require('path');
 const stubModule = require('../lib/stub-module');
 
-
 function resolve(packageName) {
-  if (require && typeof require.resolve === 'function') {
-    return path.relative(process.cwd(), path.resolve('node_modules/' + packageName));
-  }
-  // browser
-  return packageName;
+  return 'node_modules/' + packageName;
 }
 
 test('stubModule stubs some core module with subfix -browserify', t => {
-  t.deepEqual(stubModule('os'),{
+  t.deepEqual(stubModule('os', resolve),{
     name: 'os',
     location: resolve('os-browserify')
   });
@@ -20,7 +14,7 @@ test('stubModule stubs some core module with subfix -browserify', t => {
 });
 
 test('stubModule stubs domain', t => {
-  t.deepEqual(stubModule('domain'),{
+  t.deepEqual(stubModule('domain', resolve),{
     name: 'domain',
     location: resolve('domain-browser')
   });
@@ -28,7 +22,7 @@ test('stubModule stubs domain', t => {
 });
 
 test('stubModule stubs http', t => {
-  t.deepEqual(stubModule('http'),{
+  t.deepEqual(stubModule('http', resolve),{
     name: 'http',
     location: resolve('stream-http')
   });
@@ -36,7 +30,7 @@ test('stubModule stubs http', t => {
 });
 
 test('stubModule stubs querystring', t => {
-  t.deepEqual(stubModule('querystring'),{
+  t.deepEqual(stubModule('querystring', resolve),{
     name: 'querystring',
     location: resolve('querystring-es3')
   });
@@ -44,12 +38,12 @@ test('stubModule stubs querystring', t => {
 });
 
 test('stubModule ignores sys', t => {
-  t.equal(stubModule('sys'), 'define(function(){return {};});');
+  t.equal(stubModule('sys', resolve), 'define(function(){return {};});');
   t.end();
 });
 
 test('stubModule stubs zlib', t => {
-  t.deepEqual(stubModule('zlib'),{
+  t.deepEqual(stubModule('zlib', resolve),{
     name: 'zlib',
     location: resolve('browserify-zlib')
   });
@@ -57,7 +51,7 @@ test('stubModule stubs zlib', t => {
 });
 
 test('stubModule stubs fs', t => {
-  t.deepEqual(stubModule('fs'),{
+  t.deepEqual(stubModule('fs', resolve),{
     name: 'fs',
     location: resolve('fs-browser-stub')
   });
@@ -65,11 +59,11 @@ test('stubModule stubs fs', t => {
 });
 
 test('stubModule stubs empty module for some core module', t => {
-  t.equal(stubModule('dns'), 'define(function(){return {};});');
+  t.equal(stubModule('dns', resolve), 'define(function(){return {};});');
   t.end();
 });
 
 test('stubModule stubs empty module for __ignore__', t => {
-  t.equal(stubModule('__ignore__'), 'define(function(){return {};});');
+  t.equal(stubModule('__ignore__', resolve), 'define(function(){return {};});');
   t.end();
 });
