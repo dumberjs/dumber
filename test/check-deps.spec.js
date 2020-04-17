889abe011afcbea48a6ebb1f6f55722feea285ce
Chunpeng Huo <huocp@me.com>
2020-04-18 09:17:03 +1000

    feat: upgrade to stream-browserify v3

--- test/check-deps.spec.js
+++ test/check-deps.spec.js
@@ -1,7 +1,8 @@
 const test = require('tape');
-const {dependencies} = require('../package.json');
+const semver = require('semver');
 
-test('readable-stream must be 2.3.6 to make stream-browserify working', t => {
-  t.equal(dependencies['readable-stream'], '^2.3.6');
+test('readable-stream must be v3 to make stream-browserify v3 working', t => {
+  t.equal(semver.major(require('readable-stream/package.json').version), 3);
+  t.equal(semver.major(require('stream-browserify/package.json').version), 3);
   t.end();
 });
